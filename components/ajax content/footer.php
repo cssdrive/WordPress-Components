d6c58a10aca5211e7da80e86fdfc68bb02bc03a4
Fubon <thefubon@gmail.com>
2018-09-11 22:12:39 +0300

    update

--- components/ajax content/footer.php
+++ components/ajax content/footer.php
@@ -1,6 +1,6 @@
 <div id="ajax-content" class="ajax-modal uk-modal-container" uk-modal>
-	<div class="uk-modal-dialog uk-border-rounded uk-overflow-hidden">
-		<button class="uk-modal-close-default" type="button" uk-close></button>
-		<div class="ajax-response"></div>
-	</div>
+  <div class="uk-modal-dialog uk-border-rounded uk-overflow-hidden">
+    <button class="uk-modal-close-default" type="button" uk-close></button>
+    <div class="ajax-response"></div>
+  </div>
 </div>
--- components/ajax content/functions.php
+++ components/ajax content/functions.php
@@ -1,12 +1,12 @@
 <?php
 
 function ajax_content() {
-	/* Made Query */
-	$args = array( 'p' => $_POST['id'] );
-	$theme_post_query = new WP_Query( $args );
-  	while( $theme_post_query->have_posts() ) : $theme_post_query->the_post();
+  /* Made Query */
+  $args = array( 'p' => $_POST['id'] );
+  $theme_post_query = new WP_Query( $args );
+    while( $theme_post_query->have_posts() ) : $theme_post_query->the_post();
 
-      //Сюда добавляем контент цикла поста
+			//Сюда добавляем контент цикла поста
 
     endwhile;
   exit;
