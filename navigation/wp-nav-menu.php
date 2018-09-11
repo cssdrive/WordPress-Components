0975a3ab38067537169f48f8dc53a42ebee2aa22
Fubon <thefubon@gmail.com>
2018-09-11 21:25:46 +0300

    add (wp-naw-menu) & (walker-nav-menu)

---
+++ navigation/wp-nav-menu.php
@@ -0,0 +1,22 @@
+<?php
+  $nav = wp_nav_menu(array(
+    'theme_location'  => 'primary',
+    'menu'            => '',
+    'container'       => 'ul',
+    'container_class' => '',
+    'container_id'    => '',
+    'menu_class'      => 'uk-navbar-nav uk-visible@m',
+    'menu_id'         => '',
+    'echo'            => false,
+    'fallback_cb'     => false,
+    'before'          => '',
+    'after'           => '',
+    'link_before'     => '',
+    'link_after'      => '',
+    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+    'depth'           => 2,
+    'walker'          => new WalkerNavMenu('navbar'),
+  ));
+?>
+
+<?= $nav ?>
