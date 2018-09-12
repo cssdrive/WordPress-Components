a08df5a1abbb84482da7ff9a9a7ed8b279d43077
TheFubon <43171309+cssdrive@users.noreply.github.com>
2018-09-12 14:14:55 +0300

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,6 +1,4 @@
-<!--
-  Вывод списка будущих записей
--->
+<!-- Вывод списка будущих записей -->
 <?php query_posts('showposts=10&post_status=future'); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <!-- Контент -->
