1823a46b929570622debb535eb788fc7ed0483b0
TheFubon <43171309+cssdrive@users.noreply.github.com>
2018-09-13 20:31:06 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
 // Подключаем форматы записей
 add_theme_support( 'post-formats', array( 'aside', 'audio', 'image', 'video' ) );
 
