22436b49be1e80e0a2b31bc09e881e630c0c3a24
TheFubon <43171309+cssdrive@users.noreply.github.com>
2018-09-13 21:32:05 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -524,36 +524,10 @@ add_action( 'widgets_init', 'sheensay_remove_recent_comments_style' );
   Отключаем и удаляем wp-json и oembed
 -------------------------------------------------------------------*/
 
-// Отключаем WP-API версий 1.x
-add_filter( 'json_enabled', '__return_false' );
-add_filter( 'json_jsonp_enabled', '__return_false' );
- 
-// Отключаем WP-API версий 2.x
-add_filter( 'rest_enabled', '__return_false' );
-add_filter( 'rest_jsonp_enabled', '__return_false' );
- 
 // Удаляем информацию о REST API из заголовков HTTP и секции head
 remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
 remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
 remove_action( 'template_redirect', 'rest_output_link_header', 11 );
- 
-// Отключаем фильтры REST API
-remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
-remove_action( 'auth_cookie_malformed', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_expired', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_bad_username', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_bad_hash', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_valid', 'rest_cookie_collect_status' );
-remove_filter( 'rest_authentication_errors', 'rest_cookie_check_errors', 100 );
- 
-// Отключаем события REST API
-remove_action( 'init', 'rest_api_init' );
-remove_action( 'rest_api_init', 'rest_api_default_filters', 10, 1 );
-remove_action( 'parse_request', 'rest_api_loaded' );
- 
-// Отключаем Embeds связанные с REST API
-remove_action( 'rest_api_init', 'wp_oembed_register_route' );
-remove_filter( 'rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4 );
  
 // Убираем oembed ссылки в секции head
 remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
