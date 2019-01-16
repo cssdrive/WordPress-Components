e2d5177c22a9ca698cb434e78875f41c1fadf13e
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-01-16 15:15:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -28,92 +28,20 @@
 
     endif; ?>
 
+
 // FUNCTIONS -------------------------------------------
 
 add_filter( 'excerpt_length', 'change_excerpt_number_words_length' );
-/**
- * Change the number of words for the excerpts.
- *
- * @since 1.0.0
- *
- * @return int
- */
 function change_excerpt_number_words_length() {
     return 200;
 }
 
+// FUNCTIONS -------------------------------------------
 
-
-We need to see the code from the template-parts/content.php file. I suspect it is using the_content() to render out the content to the browser. You can open it and check that out.
-
-The WordPress function the_content will, by default, render out all of the content. To limit the content displayed, you have choices:
-
-    Use the_excerpt() instead of the the_content().
-    Or you'll need to write a truncate function.
-
-Using Excerpts
-
-The first option is to use the excerpts instead of grabbing all of the content. In WordPress, the excerpts are a snippet of the content with the HTML markup stripped out. By default, it gives you 55 words of content.
-
-When to Use This Option
-
-This option is the easiest as you are letting WordPress handle the truncating process. You would use this option when you don't care about preserving the HTML markup within the editor itself.
-
-How to Change the Number of Words
-
-To change the number of words, you register to the filter event and then return the number of words that you want. In this example, you are stating you want 200 words.
-
-add_filter( 'excerpt_length', 'change_excerpt_number_words_length' );
-/**
- * Change the number of words for the excerpts.
- *
- * @since 1.0.0
- *
- * @return int
- */
-function change_excerpt_number_words_length() {
-    return 200;
-}
-
-Truncate Content
-
-The second option requires you to write a function that truncates "text" to a set number of words or characters.
-
-When to Use This Option
-
-You would use this option when you want to preserve the HTML markup. Excerpts strip out the line breaks, paragraphs, etc. With this option, you are using get_the_content() and then filtering the return by truncating it. The shortcodes and HTML markup as written in the editor are preserved.
-
-How
-
-Here is where you will need some PHP and WordPress Core knowledge and chops. Essentially, you want to register a callback to the the_content filter in order to grab all of the content. Then you'll strip out the shortcodes. Finally, you'll pass it to a truncating function.
-
-This truncating function is adapted from WordPress Core:
-
-/**
- * Truncate the incoming string of text to a set number of words.
- * It preserves the HTML markup.
- *
- * @since 1.0.0
- *
- * NOTE: This function is adapted from WordPress Core's `wp_trim_words()`
- * function.  It does the same functionality, except it does not strip out
- * the HTML tag elements.
- *
- * @param string $text
- * @param int $words_limit
- * @param string $more_text
- *
- * @return string
- */
 function truncate_text( $text, $words_limit = 55, $more_text = '&hellip;' ) {
 
     $separator = ' ';
 
-    /*
-     * translators: If your word count is based on single characters (e.g. East Asian characters),
-     * enter 'characters_excluding_spaces' or 'characters_including_spaces'. Otherwise, enter 'words'.
-     * Do not translate into your own language.
-     */
     if ( strpos( _x( 'words', 'Word count type. Do not translate!' ), 'characters' ) === 0 && preg_match( '/^utf\-?8$/i', get_option( 'blog_charset' ) ) ) {
         $text = trim( preg_replace( "/[\n\r\t ]+/", ' ', $text ), ' ' );
         preg_match_all( '/./u', $text, $words_array );
@@ -132,32 +60,18 @@ function truncate_text( $text, $words_limit = 55, $more_text = '&hellip;' ) {
     return $text . $more_text;
 }
 
+// FUNCTIONS -------------------------------------------
+
 add_filter('the_content', 'truncate_the_content', 99);
-/**
- * Truncate the content to a set number of words.
- *
- * @since 1.0.0
- *
- * @param string $text
- *
- * @return string
- */
 function truncate_the_content( $text ) {
     $text = strip_shortcodes( $text );
 
     return truncate_text( $text );
 }
 
+// FUNCTIONS -------------------------------------------
+
 add_filter('the_content', 'truncate_the_content', 99);
-/**
- * Truncate the content to a set number of words.
- *
- * @since 1.0.0
- *
- * @param string $text
- *
- * @return string
- */
 function truncate_the_content( $text ) {
     if ( is_singular() ) {
         return $text;
