60c7abb0d68a495874206a9d44cbf04f4930f026
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-02-02 20:23:02 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,6 +6,35 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
+/**
+ * Вывод анонса с заданным количеством слов <p class="uk-text-lead"><?php echo excerpt(20); ?></p>
+ */
+// tn Limit Excerpt Length by number of Words
+function excerpt( $limit ) {
+$excerpt = explode(' ', get_the_excerpt(), $limit);
+if (count($excerpt)>=$limit) {
+array_pop($excerpt);
+$excerpt = implode(" ",$excerpt).'...';
+} else {
+$excerpt = implode(" ",$excerpt);
+}
+$excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+return $excerpt;
+}
+function content($limit) {
+$content = explode(' ', get_the_content(), $limit);
+if (count($content)>=$limit) {
+array_pop($content);
+$content = implode(" ",$content).'...';
+} else {
+$content = implode(" ",$content);
+}
+$content = preg_replace('/[.+]/','', $content);
+$content = apply_filters('the_content', $content);
+$content = str_replace(']]>', ']]&gt;', $content);
+return $content;
+}
+
 /*  Подключаем скрипты в head
 ============================================================*/
 
