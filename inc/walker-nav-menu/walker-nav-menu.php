a370c13f33a0cddacf11458523b4abecb5186600
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-02-06 22:52:26 +0300

    Create walker-nav-menu.php

---
+++ inc/walker-nav-menu/walker-nav-menu.php
@@ -0,0 +1,278 @@
+<?php
+	
+/**
+ * Лечим активное меню в подкатегории блога.
+ */
+function add_current_nav_class($classes, $item) {
+    global $post;
+    $current_post_type = get_post_type_object(get_post_type($post->ID));
+    $current_post_type_slug = $current_post_type->rewrite['slug'];
+    $menu_slug = strtolower(trim($item->url));
+    if (strpos($menu_slug,$current_post_type_slug) !== false) {
+        $classes[] = 'uk-active';
+    }
+    return $classes;
+}
+add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+
+/**
+ * Удаляем классы.
+ */
+function my_remove_all_class_item($classes, $item) {
+  $classes = '';
+  return $classes;
+}
+add_filter('nav_menu_css_class', 'my_remove_all_class_item', 10, 2 );
+
+/**
+ * Удаляем ID
+ */
+add_filter('nav_menu_item_id', '__return_false');
+
+/**
+ * A custom WordPress nav walker class to implement UIkit menu markup.
+ */
+class Primary_Walker_Nav_Menu extends Walker_Nav_Menu {
+    
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat( "\t", $depth );
+		$output .= "\n$indent<div class=\"uk-navbar-dropdown\" uk-drop=\"offset: 0; delay-show: 50; delay-hide: 30; duration: 50;\">\n<ul role=\"menu\" class=\"uk-nav uk-navbar-dropdown-nav uk-list-divider\">\n";
+	}
+	/**
+	 * Ends the list of after the elements are added.
+	 */
+	public function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat( "\t", $depth );
+		$output .= "\n$indent</ul></div>";
+	}
+	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+		$class_names = $value = '';
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+		//$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		if ( $args->has_children )
+			$class_names .= ' uk-active';
+		$dropdown = ' uk-parent';
+		if ( $args->has_children && $depth == 0)
+			$class_names .= ' uk-active';
+		if ( in_array( 'current-menu-item', $classes ) || in_array('current-menu-parent', $classes))
+			$class_names .= ' uk-active';
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+		$output .= $indent . '<li' . $id . $value . $class_names . $dropdown . '>';
+		$atts = array();
+		$atts['title']  = ! empty( $item->title )	? $item->title	: '';
+		$atts['target'] = ! empty( $item->target )	? $item->target	: '';
+		$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
+		// If item has_children add atts to a.
+		if ( $args->has_children && $depth === 0 ) {
+			//$atts['href'] = '#';
+			$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+			// $atts['data-toggle']   = 'dropdown';
+			// $atts['class']         = 'dropdown-toggle';
+			// $atts['aria-haspopup'] = 'true';
+		} else {
+			$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+		}
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span uk-icon="icon: chevron-down"></span></a>' : '</a>';
+		$item_output .= $args->after;
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+	/**
+	 * Traverse elements to create list from elements.
+	 *
+	 * Display one element if the element doesn't have any children otherwise,
+	 * display the element and its children. Will only traverse up to the max
+	 * depth and no ignore elements under that depth.
+	 *
+	 * This method shouldn't be called directly, use the walk() method instead.
+	 */
+	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+        if ( ! $element )
+            return;
+        $id_field = $this->db_fields['id'];
+        // Display this element.
+        if ( is_object( $args[0] ) )
+           $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
+        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    }
+	/**
+	 * Menu Fallback
+	 * =============
+	 * If this function is assigned to the wp_nav_menu's fallback_cb variable
+	 * and a menu has not been assigned to the theme location in the WordPress
+	 * menu manager the function with display nothing to a non-logged in user,
+	 * and will add a link to the WordPress menu manager if logged in as an admin.
+	 */
+	public static function fallback( $args ) {
+		if ( current_user_can( 'manage_options' ) ) {
+			extract( $args );
+			$fb_output = null;
+			if ( $container ) {
+				$fb_output = '<' . $container;
+				if ( $container_id )
+					$fb_output .= ' id="' . $container_id . '"';
+				if ( $container_class )
+					$fb_output .= ' class="' . $container_class . '"';
+				$fb_output .= '>';
+			}
+			$fb_output .= '<ul';
+			if ( $menu_id )
+				$fb_output .= ' id="' . $menu_id . '"';
+			if ( $menu_class )
+				$fb_output .= ' class="' . $menu_class . '"';
+			$fb_output .= '>';
+			$fb_output .= '<li><a href="' . admin_url( 'nav-menus.php' ) . '">' . esc_html__( 'Add a Menu', 'monopixel' ) . '</a></li>';
+			$fb_output .= '</ul>';
+			if ( $container )
+				$fb_output .= '</' . $container . '>';
+			echo $fb_output;
+		}
+	}
+}
+
+/**
+ * A custom WordPress nav walker class to implement UIkit menu markup
+ */
+class Offcanvas_Walker_Nav_Menu extends Walker_Nav_Menu {
+	
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat( "\t", $depth );
+		$output .= "\n$indent<ul role=\"menu\" class=\"uk-nav-sub uk-list-divider\">\n";
+	}
+	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+		$class_names = $value = '';
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		if ( $args->has_children )
+			$class_names .= ' uk-parent';
+		if ( in_array( 'current-menu-item', $classes )  || in_array('current-menu-parent', $classes))
+			$class_names .= ' uk-active';
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+		$output .= $indent . '<li' . $id . $value . $class_names . '>';
+		$atts = array();
+		$atts['title']  = ! empty( $item->title )	? $item->title	: '';
+		$atts['target'] = ! empty( $item->target )	? $item->target	: '';
+		$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
+		// If item has_children add atts to a.
+		if ( $args->has_children && $depth === 0 ) {
+			$atts['href']          = '#';
+			// $atts['data-toggle']   = 'dropdown';
+			// $atts['class']         = 'dropdown-toggle';
+			// $atts['aria-haspopup'] = 'true';
+		} else {
+			$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+		}
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+        if ( ! $element )
+            return;
+        $id_field = $this->db_fields['id'];
+        // Display this element.
+        if ( is_object( $args[0] ) )
+           $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
+        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    }
+	public static function fallback( $args ) {
+		if ( current_user_can( 'manage_options' ) ) {
+			extract( $args );
+			$fb_output = null;
+			if ( $container ) {
+				$fb_output = '<' . $container;
+				if ( $container_id )
+					$fb_output .= ' id="' . $container_id . '"';
+				if ( $container_class )
+					$fb_output .= ' class="' . $container_class . '"';
+				$fb_output .= '>';
+			}
+			$fb_output .= '<ul';
+			if ( $menu_id )
+				$fb_output .= ' id="' . $menu_id . '"';
+			if ( $menu_class )
+				$fb_output .= ' class="' . $menu_class . '"';
+			$fb_output .= '>';
+			$fb_output .= '<li><a href="' . admin_url( 'nav-menus.php' ) . '">' . esc_html__( 'Add a Menu', 'monopixel' ) . '</a></li>';
+			$fb_output .= '</ul>';
+			if ( $container )
+				$fb_output .= '</' . $container . '>';
+			echo $fb_output;
+		}
+	}
+}
+
+/**
+ * A custom WordPress nav walker class to implement Counter menu
+ */
+class Counter_Walker_Nav_Menu extends Walker_Nav_Menu {
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+        $class_names = $value = '';
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        if ( in_array( 'current-menu-item', $classes ) || in_array('current-menu-parent', $classes))
+			$class_names .= ' uk-active';
+        $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+        $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+        $output .= $indent . '<li' . $id . $value . $class_names .'>';
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' uk-active ' . $attr . '="' . $value . '"';
+            }
+        }
+        $item_output = $args->before;
+        $item_output .= '<a '. $attributes .'>';
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        if ($item->hasChildren && $depth == 0) {
+            $item_output .= '</a>';
+        }elseif($item->type == 'taxonomy'){
+            $cat = get_category( $item->object_id);
+            $item_output .= ' <sup style="margin-left: 3px;">'.$cat->count.'</sup></a>';
+        } else {
+            $item_output .= '</a>';
+        }
+        $item_output .= $args->after;
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+        return;
+    }
+}
