03bf34567b23ee62928e20f70735dd9f3f826fe6
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-02-18 12:44:20 +0300

    Update archive.php

--- plugins/wck/archive.php
+++ plugins/wck/archive.php
@@ -1,2 +1,24 @@
 <!-- Вывод назания категории для WCK -->
 <?php echo '<span class="">...</span>' . get_post_meta ($post->ID,'название-категории',true); ?>
+
+
+<!-- Указывается непосредственно названгие кастомной таксономии в  котрой бцдет выводится рубрики ->
+<!-- Taxonomy:* -->
+
+<?php the_terms( $post->ID, 'rubrics', 'Meeting: ', ', ', ' ' ); ?>
+
+<!-- Расширенные возможности -->
+
+<?php
+  $args = array(
+    'post_type' => 'post',
+    'tax_query' => array(
+      array(
+        'taxonomy' => 'meeting',
+        'field'    => 'slug',
+        'terms'    => 'monthly',
+      ),
+    ),
+  );
+  $query = new WP_Query( $args );
+?>
