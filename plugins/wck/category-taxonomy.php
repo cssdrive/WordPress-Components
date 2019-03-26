77f9ec80a474e79146d18831b8e8a44c08096b98
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-03-26 18:56:45 +0300

    Update category-taxonomy.php

--- plugins/wck/category-taxonomy.php
+++ plugins/wck/category-taxonomy.php
@@ -1,32 +1,8 @@
-<?php 
-	 
-	/*
-	 * Получаем все Отзывы
-	 * post_type - название нашего произвольного типа записей (идентификатор)
-	 * posts_per_page - количество получаемых записей 
-	 * (в нашем случае стоит -1, это значит, что нужно получить все посты)
-	 */
-	$params = array(
-		'tax_query' => array( // про tax_query написано чуть выще
-			array(
-				'taxonomy' => 'console', // таксономия форматов
-				'field'    => 'slug', // значение этого поля обязательно slug
-				'terms'    => array( 'nintendo-nes' ), // название одного или нескольких форматов в виде массива
-			),
-		),
-	);
-	$query = new WP_Query( $params );
-	?>
-	 
-	<div class="wrap">
- 
-    <!-- Не забудьте в цикл добавить полученный объект постов $reviews -->
-    <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
- 
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <div class="review-excerpt"><?php the_excerpt(); ?></div>
- 
-    <?php endwhile; ?>
-    <?php endif; ?>
- 
-</div>
+<?php $params = array('posts_per_page' => 4, 'orderby' => 'rand', 'tax_query' => array(array( 'taxonomy' => 'console', 'field' => 'slug', 'terms' => array( 'nintendo-nes' ),),),); $query = new WP_Query( $params ); ?>
+<?php if ( $query->have_posts() ) : ?>
+    <ul class="uk-list">
+	    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+		<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+	    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
