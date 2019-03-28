bdffa531fc4aa11cef68e50702bab25da923baf2
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-03-28 18:12:41 +0300

    add WebP Support

--- functions.php
+++ functions.php
@@ -128,6 +128,30 @@ function cc_mime_types($mimes) {
 }
 add_filter('upload_mimes', 'cc_mime_types');
 
+
+
+
+// Возможность загружать WebP
+function webp_upload_mimes( $existing_mimes ) {
+    // add webp to the list of mime types
+    $existing_mimes['webp'] = 'image/webp';
+
+    // return the array back to the function with our added mime type
+    return $existing_mimes;
+}
+add_filter( 'mime_types', 'webp_upload_mimes' );
+
+/* + Добавляем для WebP в .htaccess */
+# Serve Images with correct Mime Type
+AddType image/webp .webp
+
+# Setup Cache
+ExpiresActive On
+ExpiresByType image/webp A2592000
+	
+	
+	
+
 // Подключаем форматы записей
 add_theme_support( 'post-formats', array( 'aside', 'audio', 'image', 'video' ) );
 
